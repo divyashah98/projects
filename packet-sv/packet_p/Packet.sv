1d7eaf2dca5b28d56e16e64cce98ab4689584312
raulbehl <raulbehl@gmail.com>
2017-05-21 22:40:21 +0530

    added hton* functions

--- packet-sv/packet_p/Packet.sv
+++ packet-sv/packet_p/Packet.sv
@@ -18,6 +18,53 @@ package Packet_pkg;
         // would be overriden by every child class
         virtual function void create_packet ();
         endfunction
+
+        // Function to convert the element into
+        // big endian form. The size of the 
+        // element is 16-bits here (2 bytes)
+        function bit [15:0] htons (bit [15:0] host_pkt);
+            integer i;
+            bit [15:0] net_pkt;
+
+            net_pkt = {host_pkt[7:0], host_pkt[15:8]};
+            return net_pkt;
+        endfunction
+
+        // Function to convert the element into
+        // little endian form. The size of the 
+        // element is 16-bits here (2 bytes)
+        function bit [15:0] ntohs (bit [15:0] net_pkt);
+            integer i;
+            bit [15:0] host_pkt;
+
+            host_pkt = {net_pkt[7:0], net_pkt[15:8]};
+            return net_pkt;
+        endfunction
+
+        // Function to convert the element into
+        // big endian form. The size of the 
+        // element is 32-bits here (4 bytes)
+        function bit [31:0] htonl (bit [31:0] host_pkt);
+            integer i;
+            bit [31:0] net_pkt;
+
+             net_pkt = {host_pkt[7:0],   host_pkt[15:8],
+                        host_pkt[23:16], host_pkt[31:24]}; 
+            return net_pkt;
+        endfunction
+
+        // Function to convert the element into
+        // big endian form. The size of the 
+        // element is 48-bits here (6 bytes)
+        function bit [47:0] htonll (bit [47:0] host_pkt);
+            integer i;
+            bit [47:0] net_pkt;
+
+             net_pkt = {host_pkt[7:0],   host_pkt[15:8],
+                        host_pkt[23:16], host_pkt[31:24], 
+                        host_pkt[39:32], host_pkt[47:40]};
+            return net_pkt;
+        endfunction
     endclass
 
 endpackage
